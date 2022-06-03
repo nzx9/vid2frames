24bb752dfc85583516ad4f39f837366bc000b290
Navindu Dananga <navindudananga123@gmail.com>
2022-06-03 13:05:23 +0530

    Support to use splitter added and other minor fixes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import argparse
 from tqdm import tqdm
+import sys
 
 cmd_parser = argparse.ArgumentParser()
 
@@ -10,6 +11,9 @@ cmd_sub_parser = cmd_parser.add_subparsers(
 v2f = cmd_sub_parser.add_parser("v2f", help="Convert videos to frames")
 h5 = cmd_sub_parser.add_parser(
     "h5", help="Create hdf5 datasets from the converted frames"
+)
+split = cmd_sub_parser.add_parser(
+    "split", help="Split CSV file into train, test, and val sets"
 )
 
 v2f.add_argument(
@@ -49,6 +53,15 @@ v2f.add_argument(
     nargs="?",
     const=True,
     help="Print verbose data",
+)
+
+v2f.add_argument(
+    "-FF",
+    "--force",
+    required=False,
+    nargs="?",
+    const=True,
+    help="force frame splitter to use frames given to --frames or else it will use the best fps to automatically decide number of frames. Not setting this will return different number of frames than the given --frames",
 )
 
 h5.add_argument(
@@ -97,6 +110,62 @@ h5.add_argument(
     help="Name to save output",
 )
 
+split.add_argument(
+    "-C",
+    "--csv",
+    required=True,
+    type=str,
+    help="csv file to split",
+)
+split.add_argument(
+    "-T",
+    "--train_ratio",
+    required=True,
+    type=float,
+    help="Ratio to split train set",
+)
+split.add_argument(
+    "-V",
+    "--validate_ratio",
+    required=False,
+    type=float,
+    help="Ratio to split validation set. if not given, test set will be empty and if --train_ratio is less than 1 validate_ratio will be calculate automatically",
+)
+split.add_argument(
+    "-S",
+    "--shuffle",
+    required=False,
+    nargs="?",
+    const=True,
+    help="Shuffle the data before splitting",
+)
+
+split.add_argument(
+    "-H",
+    "--include_header",
+    required=False,
+    nargs="?",
+    const=True,
+    help="Include header to the splitting process",
+)
+
+split.add_argument(
+    "-D",
+    "--save_dir",
+    required=False,
+    type=str,
+    help="Save output csv files to this directory",
+)
+
+split.add_argument(
+    "-P",
+    "--postfix",
+    required=False,
+    type=str,
+    help="Add postfix text to output csv filename's end",
+)
+
+
 args = cmd_parser.parse_args()
 
 if __name__ == "__main__":
@@ -107,7 +176,8 @@ if __name__ == "__main__":
         frames = args.frames
         ext = args.extension
         csv = args.csv
-
+        force = False if args.force is None else args.force
+        verbose = False if args.verbose is None else args.verbose
         v2f = Vid2Frames(
             root_dir=root_dir,
             no_of_frames=frames,
@@ -116,19 +186,20 @@ if __name__ == "__main__":
         )
 
         folders = v2f.folders_in_root()
-        v2f.set_csv_header(["video_id", "label", "frames"])
+        v2f.set_csv_header(["video_id", "label", "frames", "height", "width"])
         video_id = 0
-        if args.verbose:
+        if verbose:
+            v2f.set_verbose(True)
             for folder in folders:
                 files = v2f.files_in_folder(folder=folder)
                 for file in files:
-                    v2f.split(file, video_id)
+                    v2f.split(file, video_id, force)
                     video_id += 1
         else:
             for folder in tqdm(folders):
                 files = v2f.files_in_folder(folder=folder)
                 for file in files:
-                    v2f.split(file, video_id)
+                    v2f.split(file, video_id, force)
                     video_id += 1
 
     elif args.command == "h5":
@@ -159,5 +230,16 @@ if __name__ == "__main__":
                 miss_frames_from=miss_frames_from,
             )
 
+    elif args.command == "split":
+        from src.split import Split
+
+        splitter = Split(
+            args.csv,
+            args.train_ratio,
+            args.validate_ratio,
+            args.shuffle,
+            args.include_header,
+        )
+        splitter.csv_write(args.save_dir, args.postfix)
     else:
-        print("Invalid Command selected")
+        cmd_parser.print_help(sys.stderr)
